424c558d0a4cdb367ab8a8ba16c039953e951d62
Miguel Moreno <contact@miguelmath.com>
2021-02-02 23:12:53 +0100

    project started

---
+++ linear_regresion/simple(no_technical_library).py
@@ -0,0 +1,12 @@
+#Linear regresion script with only numpy
+import numpy as np
+
+#import matplotlib to make the graphic
+import matplotlib,pyplot as plt
+
+
+def estimate_b_0_b_1:
+    n = np.size(x)
+    m_x, m_y = np.mean(x), np,mean(y)
+    
+
